2451a59fe37bb69afd100777f9e8af7ebb4e7721
Eplepai96 <bjorn.andreas1996@gmail.com>
2022-12-13 16:44:50 +0100

    Created url main file

---
+++ js/modules/callApi.mjs
@@ -0,0 +1,8 @@
+export const baseUrl = 'https://nf-api.onrender.com';
+export const registerUrl = baseUrl + `/api/v1/social/auth/register`;
+export const loginUrl = baseUrl + `/api/v1/social/auth/login`;
+
+export const postUrl = baseUrl + `/api/v1/social/post`;
+export const postsUrl = baseUrl + `/api/v1/social/posts`;
+export const updateUrl = baseUrl + `/api/v1/social/posts/`;
+export const deleteUrl = baseUrl + `/api/v1/social/posts/`;
